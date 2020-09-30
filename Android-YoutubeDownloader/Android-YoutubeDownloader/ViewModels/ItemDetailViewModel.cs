828ac1680163c8c66f15871dbdb339d12ecb80ad
Jamuel <jammmg26@gmail.com>
2020-09-30 10:29:38 +0800

    initial status

---
+++ Android-YoutubeDownloader/Android-YoutubeDownloader/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Android_YoutubeDownloader.Models;
+
+namespace Android_YoutubeDownloader.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
